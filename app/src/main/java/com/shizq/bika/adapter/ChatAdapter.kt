4ae41d82ea83cf611550e50e243651254114b9b1
shizq <shizq123hh@163.com>
2023-01-02 00:36:45 +0800

    java转kotlin

---
+++ app/src/main/java/com/shizq/bika/adapter/ChatAdapter.kt
@@ -0,0 +1,105 @@
+package com.shizq.bika.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import com.shizq.bika.R
+import com.shizq.bika.base.BaseBindingAdapter
+import com.shizq.bika.base.BaseBindingHolder
+import com.shizq.bika.bean.ChatMessageBean
+import com.shizq.bika.databinding.ItemChatBinding
+import com.shizq.bika.utils.GlideApp
+import com.shizq.bika.utils.GlideUrlNewKey
+
+class ChatAdapter :
+    BaseBindingAdapter<ChatMessageBean, ItemChatBinding>(R.layout.item_chat) {
+    override fun bindView(
+        holder: BaseBindingHolder<*, *>,
+        bean: ChatMessageBean,
+        binding: ItemChatBinding,
+        position: Int
+    ) {
+        //设置一个回复类消息的最小宽度 类似qq
+
+        if (bean.name == null && bean.user_id == null && bean.message != null) {
+            //通知 悄悄话
+            binding.chatLayoutL.visibility = View.GONE
+            binding.chatLayoutR.visibility = View.GONE
+            binding.chatNotification.visibility = View.VISIBLE
+            binding.chatNotification.text = bean.message
+        } else if (bean.type.toString() == "100") {
+            //我发送的消息
+            binding.chatLayoutL.visibility = View.GONE
+            binding.chatLayoutR.visibility = View.VISIBLE
+            binding.chatNotification.visibility = View.GONE
+            binding.chatNameR.text = bean.name
+            binding.chatContentR.text = bean.message
+        } else {
+            //接收消息
+            binding.chatLayoutL.visibility = View.VISIBLE
+            binding.chatLayoutR.visibility = View.GONE
+            binding.chatNotification.visibility = View.GONE
+            binding.chatNameL.text = bean.name
+            if (bean.character != null && bean.character != "") {
+                GlideApp.with(holder.itemView)
+                    .load(bean.character)
+                    .into(binding.chatCharacterL)
+            }
+            if (bean.avatar != null && bean.avatar != "") {
+                //拆分 利于缓存 省流量 加载更快
+                val i: Int = bean.avatar.indexOf("/static/")
+                GlideApp.with(holder.itemView)
+                    .load(
+                        if (bean.avatar != null && bean.avatar != "")
+                            if (i > 0)
+                                GlideUrlNewKey(
+                                    bean.avatar.substring(0, i),
+                                    bean.avatar.substring(i + 8)
+                                ) else bean.avatar else R.drawable.placeholder_avatar_2
+                    )
+                    .placeholder(R.drawable.placeholder_avatar_2)
+                    .into(binding.chatAvatarL)
+            }
+            if (bean.reply_name != null && bean.reply_name != "") {
+                binding.chatReplyLayout.visibility = ViewGroup.VISIBLE
+                binding.chatReplyName.text = bean.reply_name
+                if (bean.reply.length > 50) {
+                    //要改 显示两行 尾部显示...
+                    binding.chatReply.text = bean.reply.substring(0, 50) + "..."
+                } else {
+                    binding.chatReply.text = bean.reply
+                }
+            } else {
+                binding.chatReplyLayout.visibility = ViewGroup.GONE
+            }
+            if (bean.level >= 0) {
+                //等级
+                binding.chatLevelL.text = "Lv." + bean.level
+            }
+            if (bean.at != null && bean.at != "") {
+                //艾特某人
+                binding.chatAtL.visibility = View.VISIBLE
+                binding.chatAtL.text = "@" + bean.at.replace("嗶咔_", "")
+            } else {
+                binding.chatAtL.visibility = View.GONE
+            }
+
+            //显示时间 后面加设置显示隐藏
+//            String time = "";
+//            if (chatModelList.get(position).getPlatform() != null && !chatModelList.get(position).getPlatform().equals("")) {
+//                time = chatModelList.get(position).getPlatform();
+//            }
+//            time += " " + TimeUtil.getTime();
+//            holder.chat_time_l.setText(time);
+
+            if (bean.image != null && bean.image != "") {
+                //这里要处理图片
+                binding.chatContentL.setText("[有图片]")
+            } else if (bean.audio != null && bean.audio != "") {
+                //这里要处理语音
+                binding.chatContentL.setText("[有语音]")
+            } else {
+                binding.chatContentL.setText(bean.message)
+            }
+        }
+    }
+}
--- app/src/main/java/com/shizq/bika/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/shizq/bika/ui/chat/ChatActivity.kt
@@ -12,7 +12,6 @@ import com.shizq.bika.BR
 import com.shizq.bika.R
 import com.shizq.bika.adapter.ChatAdapter
 import com.shizq.bika.base.BaseActivity
-import com.shizq.bika.bean.ChatMessageBean
 import com.shizq.bika.databinding.ActivityChatBinding
 import com.shizq.bika.network.WebSocketManager
 import com.shizq.bika.widget.UserViewDialog
@@ -20,9 +19,7 @@ import com.shizq.bika.widget.UserViewDialog
 //聊天室
 //消息是websocket实现，消息是实时，不会留记录,网络不好会丢失消息
 class ChatActivity : BaseActivity<ActivityChatBinding,ChatViewModel>() {
-    private lateinit var adapter:ChatAdapter
-    private lateinit var chatModelList: ArrayList<ChatMessageBean>
-
+    private lateinit var adapterV2:ChatAdapter
     private lateinit var userViewDialog: UserViewDialog
     var chatRvBottom=false//false表示底部
 
@@ -42,10 +39,9 @@ class ChatActivity : BaseActivity<ActivityChatBinding,ChatViewModel>() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         viewModel.webSocketManager=WebSocketManager.getInstance()
 
-        chatModelList= ArrayList()
-        adapter= ChatAdapter(chatModelList)
+        adapterV2=ChatAdapter()
         binding.chatRv.layoutManager = LinearLayoutManager(this)
-        binding.chatRv.adapter = adapter
+        binding.chatRv.adapter = adapterV2
 
         userViewDialog = UserViewDialog(this)
 
@@ -75,14 +71,9 @@ class ChatActivity : BaseActivity<ActivityChatBinding,ChatViewModel>() {
             binding.chatInclude.toolbar.subtitle=it
         }
         viewModel.liveData_message.observe(this){
-            chatModelList.add(it)
-            adapter.notifyItemInserted(chatModelList.size-1)
-
-            //加个判断 当前显示的条目是否是最后一条 是就正常加载，不是就右下角显示几条消息，类似qq
-
-
+            adapterV2.addData(it)
             if (!chatRvBottom) {
-                binding.chatRv.scrollToPosition(chatModelList.size - 1)
+                binding.chatRv.scrollToPosition(adapterV2.data.size - 1)
             }
 
         }
@@ -99,14 +90,11 @@ class ChatActivity : BaseActivity<ActivityChatBinding,ChatViewModel>() {
                 }
 
             }
-
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-            }
         })
         binding.chatRvBottomBtn.setOnClickListener {
+            chatRvBottom=false
             binding.chatRvBottomBtn.visibility=View.GONE
-            binding.chatRv.scrollToPosition(chatModelList.size - 1)
+            binding.chatRv.scrollToPosition(adapterV2.data.size - 1)
         }
     }
 
