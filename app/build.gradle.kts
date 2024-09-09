09118d40298be69c63927bbff5c0e3952951b8ff
sock_magic <startxingcheng@gmail.com>
2024-09-09 21:12:14 +0800

    build(dep): add room ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
 
     implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
     implementation(libs.byrecyclerview)
