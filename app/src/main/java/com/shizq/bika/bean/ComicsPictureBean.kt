9b0f29e41291512f2830b908e6b56cfd8d7cb5a1
sock_magic <startxingcheng@gmail.com>
2025-11-19 16:51:28 +0800

    feat(data): 新增漫画列表和章节数据模型
    - 添加 `ffff` 数据类，用于表示漫画列表的 API 响应结构
    - 添加 `ComicEpisodeData` 数据类，用于表示漫画章节图片的 API 响应
    - 使用 `kotlinx.serialization` 注解 (`@Serializable`, `@SerialName`, `@JsonNames`) 进行 JSON 序列化
    - 在 `ffff` 中，使用 `@JsonNames` 同时支持 `"id"` 和 `"_id"` 字段

--- app/src/main/java/com/shizq/bika/bean/ComicsPictureBean.kt
+++ app/src/main/java/com/shizq/bika/bean/ComicsPictureBean.kt
@@ -1,5 +1,10 @@
 package com.shizq.bika.bean
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
+
+
 data class ComicsPictureBean(
     val pages: Pages,
     val ep: Ep
@@ -30,3 +35,98 @@ data class ComicsPictureBean(
     )
 }
 
+@Serializable
+data class ffff(
+    @SerialName("code")
+    val code: Int = 0,
+    @SerialName("data")
+    val `data`: Data = Data(),
+    @SerialName("message")
+    val message: String = ""
+) {
+    @Serializable
+    data class Data(
+        @SerialName("comics")
+        val comics: Comics = Comics()
+    ) {
+        @Serializable
+        data class Comics(
+            @SerialName("docs")
+            val docs: List<Doc> = listOf(),
+            @SerialName("limit")
+            val limit: Int = 0,
+            @SerialName("page")
+            val page: Int = 0,
+            @SerialName("pages")
+            val pages: Int = 0,
+            @SerialName("total")
+            val total: Int = 0
+        ) {
+            @Serializable
+            data class Doc(
+                @SerialName("author")
+                val author: String = "",
+                @SerialName("categories")
+                val categories: List<String> = listOf(),
+                @SerialName("epsCount")
+                val epsCount: Int = 0,
+                @SerialName("finished")
+                val finished: Boolean = false,
+                @JsonNames("_id", "id")
+                val id: String = "",
+                @SerialName("likesCount")
+                val likesCount: Int = 0,
+                @SerialName("pagesCount")
+                val pagesCount: Int = 0,
+                @SerialName("tags")
+                val tags: List<String> = listOf(),
+                @SerialName("thumb")
+                val media: Media = Media(),
+                @SerialName("title")
+                val title: String = "",
+                @SerialName("totalLikes")
+                val totalLikes: Int = 0,
+                @SerialName("totalViews")
+                val totalViews: Int = 0
+            )
+        }
+    }
+}
+
+@Serializable
+data class ComicEpisodeData(
+    @SerialName("ep")
+    val ep: Ep = Ep(),
+    @SerialName("pages")
+    val pages: Pages = Pages()
+) {
+    @Serializable
+    data class Ep(
+        @SerialName("_id")
+        val id: String = "",
+        @SerialName("title")
+        val title: String = ""
+    )
+
+    @Serializable
+    data class Pages(
+        @SerialName("docs")
+        val docs: List<Doc> = listOf(),
+        @SerialName("limit")
+        val limit: Int = 0,
+        @SerialName("page")
+        val page: Int = 0,
+        @SerialName("pages")
+        val pages: Int = 0,
+        @SerialName("total")
+        val total: Int = 0
+    ) {
+        @Serializable
+        data class Doc(
+            @JsonNames("_id", "id")
+            val id: String = "",
+            @SerialName("media")
+            val media: Media = Media()
+        )
+    }
+}
