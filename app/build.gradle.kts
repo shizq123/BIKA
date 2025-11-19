9291637e20e5495843e0d191de8fe052b1bad74f
sock_magic <startxingcheng@gmail.com>
2025-11-19 16:07:40 +0800

    feat: 添加 Jetpack Compose 相关依赖
    - 在 `app/build.gradle.kts` 中添加 Jetpack Compose 及其生态系统所需的核心依赖
    - 包括 `activity-compose`, `compose-material3`, `navigation-compose` 等
    - 引入 Coil 用于图片加载，以及 Kotlinx Serialization 用于 JSON 处理
    - 为后续迁移到声明式 UI 和实现自适应布局做准备

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,26 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
+    implementation(libs.androidx.compose.material3.adaptive.layout)
+    implementation(libs.androidx.compose.material3.adaptive.navigation)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.compose.runtime.tracing)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.profileinstaller)
+    implementation(libs.androidx.tracing.ktx)
+    implementation(libs.androidx.window.core)
+    implementation(libs.coil.kt)
+    implementation(libs.kotlinx.serialization.json)
+
+
+
     implementation(libs.material)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
