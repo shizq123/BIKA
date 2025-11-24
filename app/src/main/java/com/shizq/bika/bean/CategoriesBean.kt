df313b8f8d6fd77d922a56bf536ddcde49a96b48
sock_magic <startxingcheng@gmail.com>
2025-11-24 17:21:22 +0800

    refactor(bean): 重构 `CategoriesBean` 为 `kotlinx.serialization` 并拆分模型
    - 将 `CategoriesBean` 及其内部类迁移至 `kotlinx.serialization`，并添加 `@Serializable` 和 `@SerialName` 注解
    - 移除旧的、嵌套在 `CategoriesBean` 内部的 `Category` 数据类
    - 引入新的、独立的 `Category` 数据类，并为其属性（如 `active`, `description`, `_id` 等）添加默认值和序列化注解
    - 统一数据模型结构，提高代码的可维护性和序列化效率

--- app/src/main/java/com/shizq/bika/bean/CategoriesBean.kt
+++ app/src/main/java/com/shizq/bika/bean/CategoriesBean.kt
@@ -1,5 +1,9 @@
 package com.shizq.bika.bean
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
 data class CategoriesBean(
     val categories: List<Category>
 ) {
@@ -22,3 +26,28 @@ data class CategoriesBean(
         )
     }
 }
+
+@Serializable
+data class Category(
+    @SerialName("categories")
+    val categories: List<Category> = listOf()
+) {
+    @Serializable
+    data class Category(
+        @SerialName("active")
+        val active: Boolean = false,
+        @SerialName("description")
+        val description: String? = "",
+        @SerialName("_id")
+        val id: String = "",
+        @SerialName("isWeb")
+        val isWeb: Boolean = false,
+        @SerialName("link")
+        val link: String = "",
+        @SerialName("thumb")
+        val thumb: Media = Media(),
+        @SerialName("title")
+        val title: String = ""
+    )
+}
+
