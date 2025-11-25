bba946710b0d34d24a7f62b4b001043c5184addc
sock_magic <startxingcheng@gmail.com>
2025-11-25 12:55:59 +0800

    feat(network): 引入 Hilt 依赖注入模块
    - 添加 `NetworkModule` 以通过 Hilt 提供网络相关的单例对象
    - 配置 `OkHttpClient` 作为 Ktor 和 Coil 的共享网络后端
    - 提供 `Ktor HttpClient` 实例，并设置默认 API 地址和 JSON 序列化
    - 提供 `Coil ImageLoader` 实例，集成 `OkHttpClient` 并启用调试日志
    - 配置 `kotlinx.serialization.Json` 以忽略未知键，增强 API 兼容性

---
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/di/NetworkModule.kt
@@ -0,0 +1,81 @@
+package com.shizq.bika.core.network.di
+
+import android.content.Context
+import androidx.tracing.trace
+import coil3.ImageLoader
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
+import coil3.util.DebugLogger
+import com.shizq.bika.core.network.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import jakarta.inject.Singleton
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesNetworkJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun providesHttpClient(okHttpCallFactory: OkHttpClient): HttpClient = HttpClient(OkHttp) {
+        engine {
+            preconfigured = okHttpCallFactory
+        }
+        defaultRequest {
+            url("https://picaapi.picacomic.com")
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+        Logging {
+            logger = Logger.ANDROID
+            level = LogLevel.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun okHttpCallFactory(): OkHttpClient = trace("OkHttpClient") {
+        OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun imageLoader(
+        okHttpCallFactory: OkHttpClient,
+        @ApplicationContext application: Context,
+    ): ImageLoader = trace("ImageLoader") {
+        ImageLoader.Builder(application)
+            .components {
+                add(OkHttpNetworkFetcherFactory(okHttpCallFactory))
+            }
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    logger(DebugLogger())
+                }
+            }
+            .build()
+    }
+}
