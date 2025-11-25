dc6116ef35d391bcffa8f727eb56140fb953f187
sock_magic <startxingcheng@gmail.com>
2025-11-25 13:23:03 +0800

    feat(network): 自动添加哔咔漫画 API 请求头
    - 使用 Ktor `HttpSend` 插件拦截网络请求
    - 在请求发出前调用 `PicaAuth.generate` 方法生成必要的认证头信息
    - 自动将生成的 `time`, `nonce`, `signature` 等认证信息添加到每个 API 请求的 Header 中，简化网络请求逻辑

--- core/network/src/main/kotlin/com/shizq/bika/core/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/di/NetworkModule.kt
@@ -13,12 +13,16 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpSend
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.plugin
+import io.ktor.client.request.header
+import io.ktor.http.encodedPath
 import io.ktor.serialization.kotlinx.json.json
 import jakarta.inject.Singleton
 import kotlinx.serialization.json.Json
@@ -52,6 +56,19 @@ internal object NetworkModule {
         Logging {
             logger = Logger.ANDROID
             level = LogLevel.BODY
+        }
+    }.apply {
+        plugin(HttpSend).intercept { request ->
+            val path = request.url.encodedPath
+            val method = request.method.value
+
+            val headerMap = PicaAuth.generate(path, method)
+
+            headerMap.forEach { (key, value) ->
+                request.header(key, value)
+            }
+
+            execute(request)
         }
     }
 
