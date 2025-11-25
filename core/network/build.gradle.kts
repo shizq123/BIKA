e1e97f0891819ce4028d6f3955dd3e4047e039c2
sock_magic <startxingcheng@gmail.com>
2025-11-25 12:51:12 +0800

    feat(core): 新增 `network` 模块
    - 创建 `:core:network` Gradle 模块
    - 添加 Ktor, Coil, Kotlinx Serialization 等网络和序列化相关依赖
    - 在项目设置中包含新的 `network` 模块

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.bika.android.library)
+    alias(libs.plugins.bika.hilt)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.shizq.bika.core.network"
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    api(projects.core.common)
+    api(projects.core.model)
+
+    implementation(projects.core.datastore)
+
+    implementation(libs.coil.core)
+    implementation(libs.coil.network.okhttp)
+
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    implementation(libs.kotlinx.serialization.json)
+}
