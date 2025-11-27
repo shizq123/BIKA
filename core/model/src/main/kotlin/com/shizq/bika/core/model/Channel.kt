054f9e8c436736839d7c1e99800b66712e599ad1
sock_magic <startxingcheng@gmail.com>
2025-11-27 20:07:43 +0800

    refactor(model): 将 `ChannelConfig` 重命名为 `Channel` 并添加 `isActive` 字段
    - 将 `ChannelConfig` 数据类重构为 `Channel`，以更好地反映其作为数据模型的角色
    - 为 `Channel` 数据类添加 `isActive` 字段，并默认为 `true`
    - 添加 `@Serializable` 注解，为后续的序列化功能做准备

--- core/model/src/main/kotlin/com/shizq/bika/core/model/Channel.kt
+++ core/model/src/main/kotlin/com/shizq/bika/core/model/Channel.kt
@@ -1,79 +1,83 @@
 package com.shizq.bika.core.model
 
+import kotlinx.serialization.Serializable
+
 object ChannelDataSource {
     private val rawData = listOf(
         // --- App 功能区 ---
-        ChannelConfig("推荐", "ic_bika"),
-        ChannelConfig("排行榜", "ic_cat_ranking"),
-        ChannelConfig("游戏推荐", "ic_cat_game_rec"),
-        ChannelConfig("哔咔小程序", "ic_cat_mini_app"),
-        ChannelConfig("哔咔聊天室", "ic_cat_chatroom"),
-        ChannelConfig("留言板", "ic_cat_message_board"),
-        ChannelConfig("最近更新", "ic_cat_recent"),
-        ChannelConfig("随机本子", "ic_cat_random"),
+        Channel("推荐", "ic_bika"),
+        Channel("排行榜", "ic_cat_ranking"),
+        Channel("游戏推荐", "ic_cat_game_rec"),
+        Channel("哔咔小程序", "ic_cat_mini_app"),
+        Channel("哔咔聊天室", "ic_cat_chatroom"),
+        Channel("留言板", "ic_cat_message_board"),
+        Channel("最近更新", "ic_cat_recent"),
+        Channel("随机本子", "ic_cat_random"),
 
         // --- 哔咔特有分区 ---
-        ChannelConfig("援助嗶咔", "ic_cat_support"),
-        ChannelConfig("嗶咔小禮物", "ic_cat_gift"),
-        ChannelConfig("小電影", "ic_cat_movie"),
-        ChannelConfig("小里番", "ic_cat_hanime"),
-        ChannelConfig("嗶咔畫廊", "ic_cat_gallery"),
-        ChannelConfig("嗶咔商店", "ic_cat_store"),
-        ChannelConfig("大家都在看", "ic_cat_trending"),
-        ChannelConfig("大濕推薦", "ic_cat_master_choice"),
-        ChannelConfig("那年今天", "ic_cat_history"),
-        ChannelConfig("官方都在看", "ic_cat_staff_pick"),
-        ChannelConfig("嗶咔運動", "ic_cat_sport"),
-        ChannelConfig("嗶咔漢化", "ic_cat_translated"),
+        Channel("援助嗶咔", "ic_cat_support"),
+        Channel("嗶咔小禮物", "ic_cat_gift"),
+        Channel("小電影", "ic_cat_movie"),
+        Channel("小里番", "ic_cat_hanime"),
+        Channel("嗶咔畫廊", "ic_cat_gallery"),
+        Channel("嗶咔商店", "ic_cat_store"),
+        Channel("大家都在看", "ic_cat_trending"),
+        Channel("大濕推薦", "ic_cat_master_choice"),
+        Channel("那年今天", "ic_cat_history"),
+        Channel("官方都在看", "ic_cat_staff_pick"),
+        Channel("嗶咔運動", "ic_cat_sport"),
+        Channel("嗶咔漢化", "ic_cat_translated"),
 
         // --- 漫画属性 ---
-        ChannelConfig("全彩", "ic_cat_full_color"),
-        ChannelConfig("長篇", "ic_cat_long"),
-        ChannelConfig("同人", "ic_cat_doujin"),
-        ChannelConfig("短篇", "ic_cat_short"),
-        ChannelConfig("單行本", "ic_cat_tankoubon"),
-        ChannelConfig("CG雜圖", "ic_cat_cg"),
-        ChannelConfig("英語 ENG", "ic_cat_english"),
-        ChannelConfig("生肉", "ic_cat_raw"),
-        ChannelConfig("WEBTOON", "ic_cat_webtoon"),
-        ChannelConfig("歐美", "ic_cat_western"),
-        ChannelConfig("Cosplay", "ic_cat_cosplay"),
+        Channel("全彩", "ic_cat_full_color"),
+        Channel("長篇", "ic_cat_long"),
+        Channel("同人", "ic_cat_doujin"),
+        Channel("短篇", "ic_cat_short"),
+        Channel("單行本", "ic_cat_tankoubon"),
+        Channel("CG雜圖", "ic_cat_cg"),
+        Channel("英語 ENG", "ic_cat_english"),
+        Channel("生肉", "ic_cat_raw"),
+        Channel("WEBTOON", "ic_cat_webtoon"),
+        Channel("歐美", "ic_cat_western"),
+        Channel("Cosplay", "ic_cat_cosplay"),
 
         // --- 题材/标签 ---
-        ChannelConfig("純愛", "ic_cat_vanilla"),
-        ChannelConfig("百合花園", "ic_cat_yuri"),
-        ChannelConfig("耽美花園", "ic_cat_yaoi"),
-        ChannelConfig("偽娘哲學", "ic_cat_crossdress"),
-        ChannelConfig("後宮閃光", "ic_cat_harem"),
-        ChannelConfig("扶他樂園", "ic_cat_futanari"),
-        ChannelConfig("姐姐系", "ic_cat_sister_big"),
-        ChannelConfig("妹妹系", "ic_cat_sister_little"),
-        ChannelConfig("SM", "ic_cat_bdsm"),
-        ChannelConfig("性轉換", "ic_cat_gender_bender"),
-        ChannelConfig("足の恋", "ic_cat_foot"),
-        ChannelConfig("人妻", "ic_cat_milf"),
-        ChannelConfig("NTR", "ic_cat_ntr"),
-        ChannelConfig("強暴", "ic_cat_forced"),
-        ChannelConfig("非人類", "ic_cat_monster"),
-        ChannelConfig("重口地帶", "ic_cat_hardcore"),
+        Channel("純愛", "ic_cat_vanilla"),
+        Channel("百合花園", "ic_cat_yuri"),
+        Channel("耽美花園", "ic_cat_yaoi"),
+        Channel("偽娘哲學", "ic_cat_crossdress"),
+        Channel("後宮閃光", "ic_cat_harem"),
+        Channel("扶他樂園", "ic_cat_futanari"),
+        Channel("姐姐系", "ic_cat_sister_big"),
+        Channel("妹妹系", "ic_cat_sister_little"),
+        Channel("SM", "ic_cat_bdsm"),
+        Channel("性轉換", "ic_cat_gender_bender"),
+        Channel("足の恋", "ic_cat_foot"),
+        Channel("人妻", "ic_cat_milf"),
+        Channel("NTR", "ic_cat_ntr"),
+        Channel("強暴", "ic_cat_forced"),
+        Channel("非人類", "ic_cat_monster"),
+        Channel("重口地帶", "ic_cat_hardcore"),
 
         // --- IP ---
-        ChannelConfig("圓神領域", "ic_cat_madoka"),
-        ChannelConfig("碧藍幻想", "ic_cat_granblue"),
-        ChannelConfig("艦隊收藏", "ic_cat_kancolle"),
-        ChannelConfig("Love Live", "ic_cat_lovelive"),
-        ChannelConfig("SAO 刀劍神域", "ic_cat_sao"),
-        ChannelConfig("Fate", "ic_cat_fate"),
-        ChannelConfig("東方", "ic_cat_touhou"),
-        ChannelConfig("禁書目錄", "ic_cat_index")
+        Channel("圓神領域", "ic_cat_madoka"),
+        Channel("碧藍幻想", "ic_cat_granblue"),
+        Channel("艦隊收藏", "ic_cat_kancolle"),
+        Channel("Love Live", "ic_cat_lovelive"),
+        Channel("SAO 刀劍神域", "ic_cat_sao"),
+        Channel("Fate", "ic_cat_fate"),
+        Channel("東方", "ic_cat_touhou"),
+        Channel("禁書目錄", "ic_cat_index")
     )
 
-    val allChannels: List<ChannelConfig> = rawData
+    val allChannels: List<Channel> = rawData
 
-    private val lookupMap: Map<String, ChannelConfig> = rawData.associateBy { it.displayName }
+    private val lookupMap: Map<String, Channel> = rawData.associateBy { it.displayName }
 }
 
-data class ChannelConfig(
+@Serializable
+data class Channel(
     val displayName: String,
     val resName: String,
+    val isActive: Boolean = true,
 )
