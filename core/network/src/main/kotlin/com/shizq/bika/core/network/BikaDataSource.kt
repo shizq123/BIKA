236c85fbd24426ecfece6b3df8fbac155c5e7350
sock_magic <startxingcheng@gmail.com>
2025-11-27 22:52:34 +0800

    feat(network): 添加获取分类列表的 API 调用
    - 在 `BikaDataSource` 中新增 `getCategories` 方法
    - 为 `BikaDataSource` 添加 `@Singleton` 注解，确保其在依赖注入中作为单例存在

--- core/network/src/main/kotlin/com/shizq/bika/core/network/BikaDataSource.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/BikaDataSource.kt
@@ -1,9 +1,15 @@
 package com.shizq.bika.core.network
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 import jakarta.inject.Inject
+import jakarta.inject.Singleton
 
+@Singleton
 class BikaDataSource @Inject constructor(
     private val client: HttpClient,
 ) {
+    suspend fun getCategories() {
+        client.get("categories")
+    }
 }
