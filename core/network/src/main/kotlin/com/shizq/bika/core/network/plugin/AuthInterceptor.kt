f709f522e806233c664cfcf7b45bcf4d970ee673
sock_magic <startxingcheng@gmail.com>
2025-11-29 11:13:09 +0800

    refactor(network): 使用 `UserCredentialsDataSource` 获取认证令牌
    - 将 `AuthInterceptor` 的构造函数从接收一个 `tokenProvider` lambda 函数改为注入 `UserCredentialsDataSource`
    - 更新令牌的获取方式，从直接调用 lambda 改为从 `userCredentialsDataSource.userData` Flow 中异步获取
    - 使用 Hilt 的 `@Inject` 注解进行依赖注入，简化 `AuthInterceptor` 的实例化过程

--- core/network/src/main/kotlin/com/shizq/bika/core/network/plugin/AuthInterceptor.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/plugin/AuthInterceptor.kt
@@ -4,14 +4,18 @@ import coil3.intercept.Interceptor
 import coil3.network.NetworkHeaders
 import coil3.network.httpHeaders
 import coil3.request.ImageResult
+import com.shizq.bika.core.datastore.UserCredentialsDataSource
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.first
 
-class AuthInterceptor(
-    private val tokenProvider: () -> String?
+class AuthInterceptor @Inject constructor(
+    private val userCredentialsDataSource: UserCredentialsDataSource,
 ) : Interceptor {
 
     override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
         val originalRequest = chain.request
-        val token = tokenProvider()
+
+        val token = userCredentialsDataSource.userData.first().token
 
         if (token.isNullOrEmpty()) {
             return chain.proceed()
