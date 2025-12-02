3574884f5a9f62c43373fc86373ebf60166de88a
sock_magic <startxingcheng@gmail.com>
2025-12-02 15:53:38 +0800

    refactor(network): 重命名 `KeywordsResponse` 为 `KeywordsData`
    - 将 `KeywordsResponse.kt` 文件名更改为 `KeywordsData.kt`
    - 更新数据类名称为 `KeywordsData`，以更准确地反映其作为数据容器的用途

--- core/network/src/main/kotlin/com/shizq/bika/core/network/model/KeywordsData.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/model/KeywordsData.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class KeywordsResponse(
+data class KeywordsData(
     @SerialName("keywords")
     val keywords: List<String>
 )
