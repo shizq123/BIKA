8179c9efa64ca5ba475108da1addb23b2f0386cf
sock_magic <startxingcheng@gmail.com>
2025-12-02 15:53:46 +0800

    refactor(network): 为 Media 数据类属性提供默认值
    - 为 `Media` 数据类中的 `originalName`、`path` 和 `fileServer` 字段设置空字符串作为默认值
    - 增强模型的健壮性，避免在反序列化时因缺少字段而导致崩溃

--- core/network/src/main/kotlin/com/shizq/bika/core/network/model/Media.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/model/Media.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Media(
-    val originalName: String,
-    val path: String,
-    val fileServer: String
+    val originalName: String = "",
+    val path: String = "",
+    val fileServer: String = "",
 )
