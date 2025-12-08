59cb55b016adaa1b93d1f4c21601622c8ba6fd58
sock_magic <startxingcheng@gmail.com>
2025-12-08 17:05:26 +0800

    feat(database): 数据库版本升级至 3
    将 Room 数据库的版本从 2 更新到 3。

--- core/database/src/main/kotlin/com/shizq/bika/core/database/BikaDatabase.kt
+++ core/database/src/main/kotlin/com/shizq/bika/core/database/BikaDatabase.kt
@@ -16,7 +16,7 @@ import com.shizq.bika.core.database.util.InstantConverter
         HistoryRecordEntity::class,
         ReadChapterEntity::class,
     ],
-    version = 2,
+    version = 3,
     exportSchema = true,
 )
 @TypeConverters(InstantConverter::class)
