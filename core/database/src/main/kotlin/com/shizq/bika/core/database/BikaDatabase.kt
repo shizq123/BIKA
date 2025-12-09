b9a6d71614b2e548dc24ec6702eff0e2913589ea
sock_magic <startxingcheng@gmail.com>
2025-12-09 10:14:37 +0800

    refactor(database): 简化数据库模型并重构历史记录
    - 从 `BikaDatabase` 中移除 `HistoryRecordEntity` 和 `ReadChapterEntity` 实体
    - 移除不再使用的 `HistoryDao`
    - 数据库版本从 3 降至 1，以反映模型结构的重大变化

--- core/database/src/main/kotlin/com/shizq/bika/core/database/BikaDatabase.kt
+++ core/database/src/main/kotlin/com/shizq/bika/core/database/BikaDatabase.kt
@@ -3,32 +3,24 @@ package com.shizq.bika.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.shizq.bika.core.database.dao.HistoryDao
 import com.shizq.bika.core.database.dao.ReadingHistoryDao
 import com.shizq.bika.core.database.dao.RecentSearchQueryDao
 import com.shizq.bika.core.database.model.ChapterProgressEntity
 import com.shizq.bika.core.database.model.DetailedHistory
-import com.shizq.bika.core.database.model.HistoryRecordEntity
-import com.shizq.bika.core.database.model.ReadChapterEntity
 import com.shizq.bika.core.database.model.ReadingHistoryEntity
-import com.shizq.bika.core.database.model.RecentSearchQueryEntity
 import com.shizq.bika.core.database.util.InstantConverter
 
 @Database(
     entities = [
-        RecentSearchQueryEntity::class,
-        HistoryRecordEntity::class,
-        ReadChapterEntity::class,
         ReadingHistoryEntity::class,
         ChapterProgressEntity::class,
         DetailedHistory::class,
     ],
-    version = 3,
+    version = 1,
     exportSchema = true,
 )
 @TypeConverters(InstantConverter::class)
 internal abstract class BikaDatabase : RoomDatabase() {
     abstract fun recentSearchQueryDao(): RecentSearchQueryDao
-    abstract fun historyDao(): HistoryDao
     abstract fun readingHistoryDao(): ReadingHistoryDao
 }
