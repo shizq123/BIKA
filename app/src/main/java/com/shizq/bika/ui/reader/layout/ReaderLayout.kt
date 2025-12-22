6d8a7bea76078bf75bd5233b7348c81ab9326787
sock_magic <startxingcheng@gmail.com>
2025-12-22 10:32:19 +0800

    feat(reader): 调整 `ReaderLayout` 的缩放限制
    - 移除 `ZoomSpec` 中的 `minZoomFactor` 限制，恢复默认的最小缩放比例
    - 保留 `maxZoomFactor` 为 4f，确保阅读器的放大功能正常工作

--- app/src/main/java/com/shizq/bika/ui/reader/layout/ReaderLayout.kt
+++ app/src/main/java/com/shizq/bika/ui/reader/layout/ReaderLayout.kt
@@ -39,7 +39,7 @@ fun ReaderLayout(
     toggleMenuVisibility: () -> Unit
 ) {
     val scope = rememberCoroutineScope()
-    val zoomableState = rememberZoomableState(ZoomSpec(maxZoomFactor = 4f, minZoomFactor = .5f))
+    val zoomableState = rememberZoomableState(ZoomSpec(maxZoomFactor = 4f))
     val currentReaderContext by rememberUpdatedState(readerContext)
     val currentGestureState by rememberUpdatedState(gestureState)
     val currentToggleMenuVisibility by rememberUpdatedState(toggleMenuVisibility)
