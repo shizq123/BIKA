bc03bb6688c52723a0ea8e0bc23f3f1a39ede197
sock_magic <startxingcheng@gmail.com>
2025-12-27 16:18:07 +0800

    refactor(utils): 优化 `GlideUrlNewKey` 的 URL 拼接逻辑
    - 简化构造函数中的 URL 拼接，移除冗余 purchasing 的 `replace` 操作
    - 规范化代码缩进与参数换行格式，提升可读性

--- app/src/main/java/com/shizq/bika/utils/GlideUrlNewKey.kt
+++ app/src/main/java/com/shizq/bika/utils/GlideUrlNewKey.kt
@@ -3,8 +3,10 @@ package com.shizq.bika.utils
 import com.bumptech.glide.load.model.GlideUrl
 
 //因为图片的ip地址经常改变，会占用手机存储空间和浪费手机流量，所以改变Glide缓存的key,来减少占用，Glide默认缓存的key是用图片的url，现在改成url后面的参数作为缓存key
-class GlideUrlNewKey(baseUrl: String, private val key: String) : GlideUrl(
-    "${baseUrl.replace("/static/","")}/static/$key") {
+class GlideUrlNewKey(
+    baseUrl: String,
+    private val key: String
+) : GlideUrl("$baseUrl/static/$key") {
 
     //再通过重写getCacheKey() 来改变本地缓存的key
     override fun getCacheKey(): String {
