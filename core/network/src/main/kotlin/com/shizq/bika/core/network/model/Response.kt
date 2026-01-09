0b150dad6a0cbadcfaa2ece5908fafca5cb881c8
sock_magic <startxingcheng@gmail.com>
2026-01-09 16:53:08 +0800

    feat(network): 新增漫画搜索响应的数据模型
    - 在 `Response.kt` 中添加 `ComicSearchResponseData` 以承载搜索结果
    - 定义 `ComicInSearch` 数据类，用于解析搜索接口返回的漫画信息，包含 `id`, `author`, `title`, `thumb` 等字段

--- core/network/src/main/kotlin/com/shizq/bika/core/network/model/Response.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/model/Response.kt
@@ -9,3 +9,28 @@ data class ComicResource(
     @SerialName("comics")
     val comics: PageData<ComicSimple>,
 )
+
+@Serializable
+data class ComicSearchResponseData(
+    @SerialName("comics")
+    val comics: PageData<ComicInSearch>,
+)
+
+@Serializable
+data class ComicInSearch(
+    @SerialName("id")
+    val id: String,
+    val author: String,
+    val categories: List<String>,
+    val chineseTeam: String,
+    val createdAt: String,
+    val description: String,
+    val finished: Boolean,
+    val likesCount: Int,
+    val tags: List<String>,
+    val thumb: Image,
+    val title: String,
+    val totalViews: Int? = null,
+    val updatedAt: String,
+)
+// totalViews, totalLikes, pagesCount, epsCount
