fe586afab9cfa17f7cf87011e1bc4f5274721285
sock_magic <startxingcheng@gmail.com>
2026-01-11 00:05:35 +0800

    refactor(network): 统一排行榜数据模型
    - 将 `LeaderboardData` 中的 `comics` 属性类型从 `ComicDto` 更改为 `ComicSimple`，以统一数据模型并简化层级。
    - 新增 `ComicSimple` 的导入。

--- core/network/src/main/kotlin/com/shizq/bika/core/network/model/LeaderboardData.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/model/LeaderboardData.kt
@@ -1,11 +1,12 @@
 package com.shizq.bika.core.network.model
 
+import com.shizq.bika.core.model.ComicSimple
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class LeaderboardData(
-    val comics: List<ComicDto>
+    val comics: List<ComicSimple>
 )
 
 @Serializable
