19d98d13e42348bc5fe4887b2e35acb0e616643b
sock_magic <startxingcheng@gmail.com>
2026-01-10 15:22:25 +0800

    feat(core-ui): 新增通用设备预览注解 `DevicePreviews`
    - 在 `core/ui` 模块中创建 `@DevicePreviews` 注解类
    - 整合了多个 `@Preview` 注解，用于在 Compose 预览中一次性渲染多种常见设备尺寸，包括：
        - 常规手机 (phone)
        - 横向模式 (landscape)
        - 可折叠设备 (foldable)
        - 平板设备 (tablet)

---
+++ core/ui/src/main/kotlin/com/shizq/bika/core/ui/DevicePreviews.kt
@@ -0,0 +1,13 @@
+package com.shizq.bika.core.ui
+
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Multipreview annotation that represents various device sizes. Add this annotation to a composable
+ * to render various devices.
+ */
+@Preview(name = "phone", device = "spec:width=360dp,height=640dp,dpi=480")
+@Preview(name = "landscape", device = "spec:width=640dp,height=360dp,dpi=480")
+@Preview(name = "foldable", device = "spec:width=673dp,height=841dp,dpi=480")
+@Preview(name = "tablet", device = "spec:width=1280dp,height=800dp,dpi=480")
+annotation class DevicePreviews
