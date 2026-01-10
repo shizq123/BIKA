287d6fb0f2fe0418ca9cf1a92ff72fc952a30650
sock_magic <startxingcheng@gmail.com>
2026-01-10 21:05:08 +0800

    refactor(network): 统一漫画数据模型并简化 `CollectionItem`
    - 移除 `CollectionItem` 中多余的 `title` 字段
    - 将 `CollectionItem` 的 `comics` 列表类型从 `ComicDto` 替换为 `ComicSimple`
    - 删除已废弃的 `ComicDto` 数据类及其相关导入
    - 引入 `com.shizq.bika.core.model.ComicSimple` 依赖，以统一漫画数据结构

--- core/network/src/main/kotlin/com/shizq/bika/core/network/model/CollectionsData.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/model/CollectionsData.kt
@@ -1,5 +1,6 @@
 package com.shizq.bika.core.network.model
 
+import com.shizq.bika.core.model.ComicSimple
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -10,22 +11,5 @@ data class CollectionsData(
 
 @Serializable
 data class CollectionItem(
-    @SerialName("title") val title: String,
-    @SerialName("comics") val comics: List<ComicDto>
-)
-
-@Serializable
-data class ComicDto(
-    @SerialName("_id") val id: String,
-
-    @SerialName("title") val title: String,
-    @SerialName("author") val author: String,
-    @SerialName("pagesCount") val pagesCount: Int,
-    @SerialName("epsCount") val epsCount: Int,
-    @SerialName("finished") val finished: Boolean,
-    @SerialName("categories") val categories: List<String>,
-    @SerialName("tags") val tags: List<String>,
-    @SerialName("totalLikes") val totalLikes: Int,
-    @SerialName("totalViews") val totalViews: Int,
-    @SerialName("thumb") val thumb: Media
+    @SerialName("comics") val comics: List<ComicSimple>
 )
