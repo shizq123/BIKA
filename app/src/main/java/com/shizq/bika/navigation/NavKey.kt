54b92adea110750f89b799648773a757f0b72cc2
sock_magic <startxingcheng@gmail.com>
2026-01-14 23:05:15 +0800

    refactor(navigation): 为 FeedNavKey 添加 @Serializable 注解
    为 `FeedNavKey` 密封接口下的所有子类（`Collection`、`Random`、`Topic`、`Recent`、`Knight`）添加 `@Serializable` 注解，以支持序列化。

--- app/src/main/java/com/shizq/bika/navigation/NavKey.kt
+++ app/src/main/java/com/shizq/bika/navigation/NavKey.kt
@@ -8,10 +8,19 @@ object DashboardNavKey : NavKey
 
 @Serializable
 sealed interface FeedNavKey : NavKey {
+    @Serializable
     object Collection : FeedNavKey
+
+    @Serializable
     object Random : FeedNavKey
+
+    @Serializable
     data class Topic(val name: String) : FeedNavKey
+
+    @Serializable
     object Recent : FeedNavKey
+
+    @Serializable
     data class Knight(val name: String, val id: String) : FeedNavKey
 }
 
