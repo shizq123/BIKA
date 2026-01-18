95a785b9d2cd1781cc827765491ec33c6d6e2697
sock_magic <startxingcheng@gmail.com>
2026-01-18 23:46:11 +0800

    feat(app): 引入 StrictMode 和 ProfileVerifier 日志
    - 在 `BikaApplication` 的 `onCreate` 方法中为 Debug 版本启用 `StrictMode`，以检测并记录主线程上的耗时操作（如磁盘、网络访问）。
    - 注入并初始化 `ProfileVerifierLogger`，用于记录配置文件验证的状态。

--- app/src/main/java/com/shizq/bika/BikaApplication.kt
+++ app/src/main/java/com/shizq/bika/BikaApplication.kt
@@ -1,10 +1,14 @@
 package com.shizq.bika
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy.Builder
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
 import com.shizq.bika.sync.initializers.Sync
+import com.shizq.bika.util.ProfileVerifierLogger
 import com.shizq.bika.utils.SPUtil
 import dagger.hilt.android.HiltAndroidApp
 import jakarta.inject.Inject
@@ -13,12 +17,39 @@ import jakarta.inject.Inject
 class BikaApplication : Application(), SingletonImageLoader.Factory {
     @Inject
     lateinit var imageLoader: ImageLoader
+
+    @Inject
+    lateinit var profileVerifierLogger: ProfileVerifierLogger
     override fun onCreate() {
         super.onCreate()
 
+        setStrictModePolicy()
+
         SPUtil.init(this)
         Sync.initialize(this)
+        profileVerifierLogger()
     }
 
     override fun newImageLoader(context: PlatformContext): ImageLoader = imageLoader
+
+    /**
+     * Return true if the application is debuggable.
+     */
+    private fun isDebuggable(): Boolean {
+        return 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+    }
+
+    /**
+     * Set a thread policy that detects all potential problems on the main thread, such as network
+     * and disk access.
+     *
+     * If a problem is found, the offending call will be logged and the application will be killed.
+     */
+    private fun setStrictModePolicy() {
+        if (isDebuggable()) {
+            StrictMode.setThreadPolicy(
+                Builder().detectAll().penaltyLog().build(),
+            )
+        }
+    }
 }
