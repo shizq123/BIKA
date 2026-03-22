8cc3cdb1fd29321dd8b70afaaa1c3b1d8ac95f91
sock_magic <startxingcheng@gmail.com>
2026-03-22 23:16:28 +0800

    chore(model): 在 `Sort.kt` 中添加关于排序参数的错误码注释
    - 在 `Sort` 枚举类上方新增注释，记录了 API 返回 400 错误时的校验信息，明确了参数 `s` 合法的取值范围为 `[UA, CA, da, dd, ld, vd]`。

--- core/model/src/main/kotlin/com/shizq/bika/core/model/Sort.kt
+++ core/model/src/main/kotlin/com/shizq/bika/core/model/Sort.kt
@@ -3,7 +3,7 @@ package com.shizq.bika.core.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
-
+//{"code":400,"error":"1002","message":"validation error","detail":"s must be one of [UA, CA, da, dd, ld, vd]"}
 @Serializable
 enum class Sort(val value: String) {
     /** 最新 */
