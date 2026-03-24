81ce90002f43860522a48557c1e28c0338cbdd44
sock_magic <startxingcheng@gmail.com>
2026-03-24 16:34:04 +0800

    refactor(network): 将 `Comment` 及其相关数据模型移出 `CommentDoc` 提升为顶层类
    - 将 `Comment` 数据类从 `CommentDoc` 的内部类改为顶层类。
    - `ComicMiniInfo` 维持作为 `Comment` 的嵌套类。
    - 此项改动有助于简化模型引用并提高代码的可维护性。

--- core/network/src/main/kotlin/com/shizq/bika/core/network/model/CommentDoc.kt
+++ core/network/src/main/kotlin/com/shizq/bika/core/network/model/CommentDoc.kt
@@ -8,34 +8,34 @@ import kotlinx.serialization.json.JsonNames
 data class CommentDoc(
     @SerialName("comments")
     val comments: PageData<Comment>
+)
+
+@Serializable
+data class Comment(
+    @SerialName("_comic")
+    val comic: ComicMiniInfo = ComicMiniInfo(),
+    @SerialName("commentsCount")
+    val commentsCount: Int = 0,
+    @SerialName("content")
+    val content: String = "",
+    @SerialName("created_at")
+    val createdAt: String = "",
+    @SerialName("hide")
+    val hide: Boolean = false,
+    @JsonNames("_id", "id")
+    val id: String = "",
+    @SerialName("isLiked")
+    val isLiked: Boolean = false,
+    @SerialName("likesCount")
+    val likesCount: Int = 0,
+    @SerialName("totalComments")
+    val totalComments: Int = 0
 ) {
     @Serializable
-    data class Comment(
-        @SerialName("_comic")
-        val comic: ComicMiniInfo = ComicMiniInfo(),
-        @SerialName("commentsCount")
-        val commentsCount: Int = 0,
-        @SerialName("content")
-        val content: String = "",
-        @SerialName("created_at")
-        val createdAt: String = "",
-        @SerialName("hide")
-        val hide: Boolean = false,
-        @JsonNames("_id", "id")
+    data class ComicMiniInfo(
+        @SerialName("_id")
         val id: String = "",
-        @SerialName("isLiked")
-        val isLiked: Boolean = false,
-        @SerialName("likesCount")
-        val likesCount: Int = 0,
-        @SerialName("totalComments")
-        val totalComments: Int = 0
-    ) {
-        @Serializable
-        data class ComicMiniInfo(
-            @SerialName("_id")
-            val id: String = "",
-            @SerialName("title")
-            val title: String = ""
-        )
-    }
+        @SerialName("title")
+        val title: String = ""
+    )
 }
