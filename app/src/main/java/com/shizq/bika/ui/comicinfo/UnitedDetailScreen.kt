533358c61666baafcd3780ac615585b68d0a9e24
STlxx <1414794992@qq.com>
2026-06-21 03:22:14 +0800

    feat: implement UnitedDetailScreen for comic details, episodes, and comment management

--- app/src/main/java/com/shizq/bika/ui/comicinfo/UnitedDetailScreen.kt
+++ app/src/main/java/com/shizq/bika/ui/comicinfo/UnitedDetailScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
