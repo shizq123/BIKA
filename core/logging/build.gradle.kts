fe333078c2009ed224a63914f16579f5016aa146
sock_magic <startxingcheng@gmail.com>
2026-08-30 00:18:27 +0800

    更新

--- core/logging/build.gradle.kts
+++ core/logging/build.gradle.kts
@@ -9,6 +9,9 @@ android {
 dependencies {
     api(libs.kotlin.logging)
 
-    implementation(libs.log4j.slf4j2.impl)
     implementation(libs.log4j.core)
+    implementation(libs.log4j.slf4j.impl)
+    implementation(libs.slf4j.api)
+    implementation(libs.slf4j.simple)
+    implementation("com.github.tony19:logback-android:3.0.0")
 }
