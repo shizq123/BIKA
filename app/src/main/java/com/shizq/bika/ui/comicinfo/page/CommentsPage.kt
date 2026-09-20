2982122c8486dfb615caf219246ae7fbb3ddfaac
sock_magic <startxingcheng@gmail.com>
2026-09-20 23:40:30 +0800

    更新

--- app/src/main/java/com/shizq/bika/ui/comicinfo/page/CommentsPage.kt
+++ app/src/main/java/com/shizq/bika/ui/comicinfo/page/CommentsPage.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
